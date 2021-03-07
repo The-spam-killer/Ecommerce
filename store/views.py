fa7adebb7c24104b7afb82e5624f6d9d386c686f
Mostafa <Mustafabarahmeh555@gmail.com>
2021-03-07 09:06:14 +0200

    Second Push

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
@@ -11,13 +12,31 @@ def index(request):
 
 
 def store(request):
-    context = {}
-    return render(request,'store/store.html')
+    products = Product.objects.all()
+    context = {"products":products}
+    return render(request,'store/store.html',context)
 
 def cart(request):
-    context = {}
-    return render(request,'store/cart.html')
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order,created = Order.objects.get_or_create(customer= customer)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {"get_cart_items":0,"get_cart_total":0}
+
+    context = {'items':items,'order':order}
+    return render(request,'store/cart.html',context)
 
 def checkout(request):
-    context = {}
-    return render(request,'store/checkout.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order,created = Order.objects.get_or_create(customer= customer)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {"get_cart_items":0,"get_cart_total":0}
+
+    context = {'items':items,'order':order}
+    return render(request,'store/checkout.html',context)
